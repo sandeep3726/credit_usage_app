2e8afa1dd8a10b0d49e788c946a9a1bf0edd5bba
sandeep3726 <124679032+sandeep3726@users.noreply.github.com>
2023-02-23 14:59:18 +0530

    Update credit_usage.py

--- credit_usage.py
+++ credit_usage.py
@@ -54,7 +54,4 @@ df2 = pd.read_sql(sql2,con=connector)
 st.table(df1)
 st.bar_chart(data = df2, x= "DAY", y = "CREDIT_CONSUMPTION")
 
-st.subheader("credit consumption")
-fig = px.bar(data_frame = df2, x="day", y="credit_consumption",template = "simple_white")
-fig.update_layout()        
-st.plotly_chart(fig,use_container_width=True)
+
